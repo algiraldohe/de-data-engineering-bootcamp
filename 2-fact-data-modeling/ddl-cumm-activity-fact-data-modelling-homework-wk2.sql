321333a95641ed747c6db72811f8df67a5c1aa1c
Alejandro G <algiraldohe@gmail.com>
2025-07-10 18:56:21 +0200

    cumulative table and incremental query

---
+++ 2-fact-data-modeling/ddl-cumm-activity-fact-data-modelling-homework-wk2.sql
@@ -0,0 +1,44 @@
+-- DDL for cummulative table
+-- DROP TABLE user_devices_cumulated;
+CREATE TABLE user_devices_cumulated (
+	user_id TEXT,
+	browser_type TEXT,
+	device_activity_datelist DATE[],
+	date DATE,
+	PRIMARY KEY (user_id, browser_type, date)
+);
+
+-- Incremmental query to populate user_devices_cumulated
+WITH yesterday AS (
+	SELECT * FROM user_devices_cumulated
+	WHERE date = DATE('2023-01-30')
+),
+
+today AS (
+	SELECT
+		e.user_id::TEXT AS user_id,
+		d.browser_type,
+		DATE(e.event_time::TIMESTAMP) AS  date_active
+
+	FROM events e
+	INNER JOIN devices d ON  e.device_id = d.device_id
+	WHERE
+		e.user_id  IS NOT NULL
+		AND d.device_id IS NOT NULL
+		AND DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-31')
+	GROUP BY 1,2,3
+)
+
+INSERT INTO user_devices_cumulated (
+SELECT
+	COALESCE(t.user_id, y.user_id) AS  user_id,
+	COALESCE(t.browser_type, y.browser_type) AS browser_type,
+	CASE WHEN y.device_activity_datelist IS NULL THEN ARRAY[t.date_active]
+		 WHEN t.date_active IS NULL THEN y.device_activity_datelist
+		 WHEN y.device_activity_datelist IS NOT NULL THEN ARRAY[t.date_active] || y.device_activity_datelist
+		END AS dates_active,
+	COALESCE(t.date_active, y.date + INTERVAL '1 day') AS  date
+
+FROM today t
+FULL OUTER JOIN yesterday y ON y.user_id = t.user_id AND y.browser_type = t.browser_type
+);
